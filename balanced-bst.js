1469b455e9aab88c6089a65fa6ba64d8229d947f
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-09 10:54:09 -0500

    Change the midpoint to taking the ceiling of the average of start-end to maintain value hierarchy

--- balanced-bst.js
+++ balanced-bst.js
@@ -6,15 +6,16 @@ const Node = (d) => {
 }
 
 const Tree = (array) => {
-    const sortedArray = removeDuplicates(array.sort());
-    const root = buildTree(sortedArray, 0, sortedArray.length-1);
+    const sortedArray = array.sort(function(a, b){return a-b});
+    const sortedArrayNoDups = removeDuplicates(sortedArray);
+    const root = buildTree(sortedArrayNoDups, 0, sortedArrayNoDups.length-1);
     return {root};
 }
 
 function buildTree(array, start, end) {
     if (start > end) return null;
 
-    const midpoint = parseInt((start + end) / 2);
+    const midpoint = Math.ceil((start + end) / 2);
     const node = Node(array[midpoint]);
     
     node.left = buildTree(array, start, midpoint-1);
@@ -27,7 +28,7 @@ function removeDuplicates(sortedArray) {
     let reducedArray = [];
 
     for (value of sortedArray) {
-        if (value in reducedArray) continue;
+        if (reducedArray.includes(value)) continue;
         reducedArray.push(value);
     }
 
@@ -43,7 +44,10 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
   }
-  
-let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+// let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let array = [1, 3,4,6,7,8,10,13,14];
 let newTree = Tree(array);
 prettyPrint(newTree.root);
+// let sortedArray = array.sort(function(a, b){return a-b});
+// console.log(removeDuplicates(sortedArray));
