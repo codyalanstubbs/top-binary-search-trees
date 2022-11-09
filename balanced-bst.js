f3eee4cc6d23b39b3001bf5828ba15f8621f181e
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-09 10:09:11 -0500

    Add Tree factory and functions to prepare array and build tree

--- balanced-bst.js
+++ balanced-bst.js
@@ -4,8 +4,46 @@ const Node = (d) => {
     const right = null;
     return { data, left, right }
 }
-// let newNode = Node(1);
-// console.log(newNode);
-// console.log(newNode.data);
-// console.log(newNode.left);
-// console.log(newNode.right);
+
+const Tree = (array) => {
+    const sortedArray = removeDuplicates(array.sort());
+    const root = buildTree(sortedArray, 0, sortedArray.length-1);
+    return {root};
+}
+
+function buildTree(array, start, end) {
+    if (start > end) return null;
+
+    const midpoint = parseInt((start + end) / 2);
+    const node = Node(array[midpoint]);
+    
+    node.left = buildTree(array, start, midpoint-1);
+    node.right = buildTree(array, midpoint+1, end);
+
+    return node;
+}
+
+function removeDuplicates(sortedArray) {
+    let reducedArray = [];
+
+    for (value of sortedArray) {
+        if (value in reducedArray) continue;
+        reducedArray.push(value);
+    }
+
+    return reducedArray;
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+  
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let newTree = Tree(array);
+prettyPrint(newTree.root);
