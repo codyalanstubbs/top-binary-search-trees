e7a3e92c59c2faeae81259f38fe493839561d18d
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-09 09:30:49 -0500

    Add a Node factory function

---
+++ balanced-bst.js
@@ -0,0 +1,11 @@
+const Node = (d) => {
+    const data = d;
+    const left = null;
+    const right = null;
+    return { data, left, right }
+}
+// let newNode = Node(1);
+// console.log(newNode);
+// console.log(newNode.data);
+// console.log(newNode.left);
+// console.log(newNode.right);
